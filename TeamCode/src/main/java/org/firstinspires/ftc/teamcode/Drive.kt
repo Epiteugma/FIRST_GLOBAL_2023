45c633761758ccec077692106175dfaca9064eeb
z3db0y <us3r0002@gmail.com>
2023-09-23 11:39:46 +0000

    Add servo controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.kt
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit
 import org.firstinspires.ftc.robotcore.external.navigation.VoltageUnit
 import kotlin.concurrent.thread
@@ -15,10 +16,44 @@ class Drive: LinearOpMode() {
     private val SLIDE_MLT = 1.0
     private val HOLD_POWER = 1.0
 
+    // TODO: tune these
+    private val FILTER_UPWARDS = arrayOf(0.0, 0.0)
+    private val FILTER_DOWNWARDS = arrayOf(1.0, 1.0)
+    private val FILTER_CENTER = arrayOf(0.5, 0.5)
+
+    private val STORAGE_CLOSED = arrayOf(0.0, 0.0)
+    private val STORAGE_OPEN = arrayOf(1.0, 1.0)
+
+    // Hardware Devices
     private lateinit var left: DcMotor
     private lateinit var right: DcMotor
     private lateinit var leftSlide: DcMotor
     private lateinit var rightSlide: DcMotor
+
+    private lateinit var filterServoLeft: Servo
+    private lateinit var filterServoRight: Servo
+    private lateinit var storageServoLeft: Servo
+    private lateinit var storageServoRight: Servo
+
+    // Servo State Enums
+    private enum class FilterState {
+        NONE,
+        UPWARDS,
+        DOWNWARDS,
+        CENTER;
+    }
+
+    private enum class StorageState {
+        NONE,
+        OPEN,
+        CLOSED;
+    }
+
+    // Servo State Variables
+    private var filterState = FilterState.NONE
+    private var lastFilterState = FilterState.NONE
+    private var storageState = StorageState.NONE
+    private var lastStorageState = StorageState.NONE
 
     override fun runOpMode() {
         // Hardware Initialization
@@ -31,7 +66,7 @@ class Drive: LinearOpMode() {
         left.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
         right.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
 
-        var initTime = System.currentTimeMillis()
+        val initTime = System.currentTimeMillis()
         var lastDotsIncrement = System.currentTimeMillis()
         var dots = 0
 
@@ -46,7 +81,7 @@ class Drive: LinearOpMode() {
             }
         }
 
-        var startTime = System.currentTimeMillis()
+        val startTime = System.currentTimeMillis()
 
         // Driver 1 Thread
         thread(name = "DRIVER1", start = true) {
@@ -67,6 +102,42 @@ class Drive: LinearOpMode() {
                     motor.mode = DcMotor.RunMode.RUN_TO_POSITION
                     motor.power = HOLD_POWER
                 }
+
+                if(lastFilterState != filterState) {
+                    when(filterState) {
+                        FilterState.UPWARDS -> {
+                            filterServoLeft.position = FILTER_UPWARDS[0]
+                            filterServoRight.position = FILTER_UPWARDS[1]
+                        }
+                        FilterState.DOWNWARDS -> {
+                            filterServoLeft.position = FILTER_DOWNWARDS[0]
+                            filterServoRight.position = FILTER_DOWNWARDS[1]
+                        }
+                        FilterState.CENTER -> {
+                            filterServoLeft.position = FILTER_CENTER[0]
+                            filterServoRight.position = FILTER_CENTER[1]
+                        }
+                        else -> {}
+                    }
+
+                    lastFilterState = filterState
+                }
+
+                if(lastStorageState != storageState) {
+                    when(storageState) {
+                        StorageState.OPEN -> {
+                            storageServoLeft.position = STORAGE_OPEN[0]
+                            storageServoRight.position = STORAGE_OPEN[1]
+                        }
+                        StorageState.CLOSED -> {
+                            storageServoLeft.position = STORAGE_CLOSED[0]
+                            storageServoRight.position = STORAGE_CLOSED[1]
+                        }
+                        else -> {}
+                    }
+
+                    lastStorageState = storageState
+                }
             }
         }
 
@@ -78,11 +149,20 @@ class Drive: LinearOpMode() {
             telemetry.addData("", "")
 
             telemetry.addData("--- SLIDES ---", "")
-            telemetry.addData("Mode", leftSlide.mode.toString())
+            telemetry.addData("Mode", leftSlide.mode)
             if(leftSlide.mode == DcMotor.RunMode.RUN_TO_POSITION) {
                 telemetry.addData("Left target", leftSlide.targetPosition)
                 telemetry.addData("Right target", rightSlide.targetPosition)
             } else telemetry.addData("Power", leftSlide.power)
+            telemetry.addData("", "")
+
+            telemetry.addData("--- SERVOS ---", "")
+            telemetry.addData("Storage state", storageState)
+            telemetry.addData("Left Storage Servo", storageServoLeft.position)
+            telemetry.addData("Right Storage Servo", storageServoRight.position)
+            telemetry.addData("Filter state", filterState)
+            telemetry.addData("Left Filter Servo", filterServoLeft.position)
+            telemetry.addData("Right Filter Servo", filterServoRight.position)
             telemetry.addData("", "")
 
             telemetry.addData("--- HEALTH ---", "")
